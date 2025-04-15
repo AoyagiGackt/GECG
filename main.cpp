87253b4242b8b05980e07b47e2eae8e87c6dea91
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-04-15 13:00:37 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
-﻿#include <Windows.h>
+﻿/*———————————–———————————–—————————–——————
+*  include
+———————————–———————————–—————————–————————*/
+#include <Windows.h>
+#include <cstdint>
+#include <format>
+#include <string>
+// ファイルやディレクトリに関する操作を行うライブラリ
+#include <filesystem>
+// ファイルに書いたり読んだりするライブラリ
+#include <fstream>
+// 時間を扱うライブラリ
+#include <chrono>
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
