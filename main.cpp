fd93eadde480d936c1c7642a36167d7d491b63c2
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-04-22 12:33:04 +0900

    a

--- main.cpp
+++ main.cpp
@@ -156,12 +156,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // いい順にアダプタを頼む
     for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i) {
         // アダプターの情報を取得する
-        DXGI_ADAPTER_DESC1 adapterDesc{};
+        DXGI_ADAPTER_DESC3 adapterDesc {};
         hr = useAdapter->GetDesc3(&adapterDesc);
         assert(SUCCEEDED(hr));
+
         // ソフトウェアアダプタでなければ採用!
         if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)) {
-            Log(ConvertString(std::format(L"Adapater:{}\n", adapterDesc.Description)));
+            Log(ConvertString(std::format(L"USE Adapter:{}\n", adapterDesc.Description)));
             break;
         }
         useAdapter = nullptr; // ソフトウェアアダプタの場合は見なかったことにする
