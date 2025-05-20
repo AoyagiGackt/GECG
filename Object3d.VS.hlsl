72e5e9e6b880de4259064e49d28e93ee31341ad0
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-05-20 10:50:31 +0900

    t4

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -6,12 +6,12 @@ float4 main( float4 pos : POSITION ) : SV_POSITION
 // float32_t4
 struct VertexShaderOutput
 {
-    float32_t4 position : SV_POSITION;
+    float4 position : SV_POSITION;
 };
 
 struct VertexShaderInput
 {
-    float32_t4 position : POSITION0;
+    float4 position : POSITION0;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
@@ -23,13 +23,13 @@ VertexShaderOutput main(VertexShaderInput input)
 
 struct PixelShaderOutput
 {
-    float32_t4 color : SV_TARGET0;
+    float4 color : SV_TARGET0;
 };
 
 PixelShaderOutput main()
 {
     PixelShaderOutput output;
-    output.color = float32_t(1.0, 1.0, 1.0, 1.0);
+    output.color = float4(1.0, 1.0, 1.0, 1.0);
     return output;
 }
 
