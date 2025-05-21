f1aa81d875150f897287adcdcd1c08ad62fcca51
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-05-21 14:38:06 +0900

    ９

--- Object.3dPS.hlsl
+++ Object.3dPS.hlsl
@@ -1,6 +1,20 @@
+
+struct Vector4
+{
+    float x, y, z, w;
+};
+
+struct Material
+{
+    Vector4 color;
+};
+
+ConstantBuffer<Material> gMaterial : register(b0);
+
 struct PixelShaderOutput
 {
     float4 color : SV_TARGET0;
+    float4 color : SV_TARGET;
 };
 
 PixelShaderOutput main()
