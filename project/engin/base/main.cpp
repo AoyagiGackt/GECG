0721a7378ba2a0c3e960238688f3d1a7ab346726
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-10-31 11:53:56 +0900

    03-01ちょっと02入っちゃってます

--- project/engin/base/main.cpp
+++ project/engin/base/main.cpp
@@ -4,6 +4,7 @@
 
 #include "DirectXTex.h"
 #include "Input.h"
+#include "WinApp.h"
 #include "MakeAffine.h"
 #include "ResourceObject.h"
 #include "d3dx12.h"
@@ -386,52 +387,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
     ComPtr<ID3D12Device> device;
 
+    // ポインタ
+    WinApp* winApp = nullptr;
+
+    // windowsAPIの初期化
+    winApp = new WinApp();
+    winApp->Initialize();
+
     CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-
-    WNDCLASS wc = {};
-
-    // ウィンドクラスプロシージャ
-    wc.lpfnWndProc = WindowProc;
-
-    // ウィンドクラス名
-    wc.lpszClassName = L"CG2WindowClass";
-
-    // インスタンスハンドル
-    wc.hInstance = GetModuleHandleW(nullptr);
-
-    // カーソル
-    wc.hCursor = LoadCursorW(nullptr, IDC_ARROW);
-
-    // ウィンドクラスの登録
-    RegisterClassW(&wc);
-
-    // クライアント領域のサイズ
-    const int32_t kClientWidth = 1280;
-    const int32_t kClientHeight = 720;
-
-    // ウィンドウサイズを表す構造体にクライアント領域を入れる
-    RECT wrc = { 0, 0, kClientWidth, kClientHeight };
-
-    // クライアント領域をもとに実際のサイズにwrcを変更
-    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-    // ウィンドウの生成
-    HWND hwnd = CreateWindow(
-        wc.lpszClassName, // 利用するクラス名
-        L"CG2", // タイトルバーの文字
-        WS_OVERLAPPEDWINDOW, // よく見るウィンドウスタイル
-        CW_USEDEFAULT, // 表示x座標(Windowsに任せる)
-        CW_USEDEFAULT, // 表示y座標(WindowsOSに任せる)
-        wrc.right - wrc.left, // ウィンドウ横幅
-        wrc.bottom - wrc.top, // ウィンドウ縦幅
-        nullptr, // 親ウィンドウハンドル
-        nullptr, // メニューハンドル
-        wc.hInstance, // インスタンスハンドル
-        nullptr // オプション
-    );
-
-    // ウィンドウを表示する
-    ShowWindow(hwnd, SW_SHOW);
 
     MSG msg {};
 
@@ -1423,9 +1386,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
     CoUninitialize();
     
-    CloseWindow(hwnd);
+    // delete input;
+    // delete winApp
 
-    //delete input;
+    CloseWindow(hwnd);
 
     // --------------------------------------------------
     // デバッグ用リソースリークチェック
---
+++ project/engin/game/h/WinApp.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <Windows.h>
+
+// WindowsAPI
+class WinApp {
+public: // 静的メンバ関数
+    // ウィンドウプロシージャ
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+public: // メンバ関数
+    // 初期化
+    void Initialize();
+    // 更新
+    void Update();
+};
