614b1288081a7a807588b2181a983c3ba06fbaa8
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-11-10 16:14:47 +0900

    　

--- project/engin/game/h/DirectXCommon.h
+++ project/engin/game/h/DirectXCommon.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
+#include <cassert>
 #include <d3d12.h>
 #include <d3dx12.h>
-#include <dxcapi.h>
 #include <dxgi1_6.h>
 #include <wrl.h>
 
@@ -11,7 +11,36 @@ class WinApp;
 class DirectXCommon {
 public: // メンバ関数
     // 初期化
-    void Initialize();
+    void Initialize(WinApp* winApp);
+
+    ID3D12Device* GetDevice() { return device_.Get(); }
+    ID3D12GraphicsCommandList* GetCommandList() { return commandList_.Get(); }
+    ID3D12CommandQueue* GetCommandQueue() { return commandQueue_.Get(); }
+    IDXGISwapChain4* GetSwapChain() { return swapChain_.Get(); }
+    ID3D12CommandAllocator* GetCommandAllocator() { return commandAllocator_.Get(); }
+    ID3D12DescriptorHeap* GetSrvDescriptorHeap() { return srvDescriptorHeap_.Get(); }
+
+    // RTV関連
+    D3D12_CPU_DESCRIPTOR_HANDLE GetCurrentBackBufferHandle();
+    ID3D12Resource* GetCurrentBackBufferResource();
+    DXGI_FORMAT GetBackBufferFormat() const { return DXGI_FORMAT_R8G8B8A8_UNORM_SRGB; }
+    UINT GetBufferCount() const { return 2; }
+    D3D12_CPU_DESCRIPTOR_HANDLE GetDsvHandle() { return dsvDescriptorHeap_->GetCPUDescriptorHandleForHeapStart(); }
+
+    // フェンス関連
+    ID3D12Fence* GetFence() { return fence_.Get(); }
+    uint64_t GetFenceValue() const { return fenceValue_; }
+    HANDLE GetFenceEvent() { return fenceEvent_; }
+    void IncrementFenceValue() { fenceValue_++; }
+
+private:
+    void InitializeDevice();
+    void CreateCommand();
+    void CreateSwapChain();
+    void CreateDescriptorHeaps();
+    void CreateRTV();
+    void CreateDepthBuffer();
+    void CreateFence();
 
 private:
     Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory_;
@@ -23,8 +52,12 @@ private:
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvDescriptorHeap_;
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> srvDescriptorHeap_;
     Microsoft::WRL::ComPtr<ID3D12Resource> swapChainResoures_[2];
+    Microsoft::WRL::ComPtr<ID3D12Resource> depthStencilResource_;
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> dsvDescriptorHeap_;
     D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles_[2];
     Microsoft::WRL::ComPtr<ID3D12Fence> fence_;
     uint64_t fenceValue_;
     HANDLE fenceEvent_;
+
+    WinApp* winApp_ = nullptr;
 };
