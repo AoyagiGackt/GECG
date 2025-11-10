5948d740a9e0b4fcc328216b0968ba7b557f6fe3
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-11-10 16:23:55 +0900

    　

--- project/engin/game/h/StringUtlity.h
+++ project/engin/game/h/StringUtlity.h
@@ -1,3 +1,8 @@
 #pragma once
-class StringUtlity {
+#include <string>
+
+// 文字コードゆーてぃりてぃ
+namespace StringUtility {
+std::wstring ConvertString(const std::string& str);
+std::string ConvertString(const std::wstring& str);
 };
