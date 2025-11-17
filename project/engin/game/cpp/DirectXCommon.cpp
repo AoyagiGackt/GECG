5f3fc1c732b8d0a51840fd1db36319a1d424c80f
AoyagiGakuto <youdesugakkou@gmail.com>
2025-11-17 15:09:25 +0900

    21

--- project/engin/game/cpp/DirectXCommon.cpp
+++ project/engin/game/cpp/DirectXCommon.cpp
@@ -295,6 +295,35 @@ void DirectXCommon::CreateFence()
     assert(fenceEvent_ != nullptr);
 }
 
+void DirectXCommon::InitializeFixFPS()
+{
+    // 現在時間を記録する
+    reference_ = std::chrono::steady_clock::now();
+}
+
+void DirectXCommon::UpdateFixFPS()
+{
+    // 1/60秒ぴったりの時間
+    const std::chrono::microseconds kMinTime(uint64_t(1000000.0f) / 60.0f);
+    // 1/60秒よりわずかに短い時間
+    const std::chrono::microseconds kMinCheckTime(uint64_t(1000000.0f) / 65.0f);
+    // 現在時間を取得
+    std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+    // 前回記録からの経過時間を取得する
+    std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
+
+    // 1/60(よりわずかに短い時間)立っていない場合
+    if (elapsed < kMinTime) {
+        // 1/60秒になるまで微小なスリープを繰り返す
+        while (std::chrono::steady_clock::now() - reference_ < kMinTime) {
+            // 1マイクロ秒スリープ
+            std::this_thread::sleep_for(std::chrono::microseconds(1));
+        }
+    }
+    // 現在時間を記録する
+    reference_ = std::chrono::steady_clock::now();
+}
+
 D3D12_CPU_DESCRIPTOR_HANDLE DirectXCommon::GetCurrentBackBufferHandle()
 {
     UINT backBufferIndex = swapChain_->GetCurrentBackBufferIndex();
