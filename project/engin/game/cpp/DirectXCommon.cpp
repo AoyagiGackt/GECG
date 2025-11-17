e3c6f8b484a29456327001ed60799e16fe20c01f
AoyagiGakuto <youdesugakkou@gmail.com>
2025-11-17 15:16:11 +0900

    60Fps

--- project/engin/game/cpp/DirectXCommon.cpp
+++ project/engin/game/cpp/DirectXCommon.cpp
@@ -313,7 +313,7 @@ void DirectXCommon::UpdateFixFPS()
     std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
 
     // 1/60(よりわずかに短い時間)立っていない場合
-    if (elapsed < kMinTime) {
+    if (elapsed < kMinCheckTime) {
         // 1/60秒になるまで微小なスリープを繰り返す
         while (std::chrono::steady_clock::now() - reference_ < kMinTime) {
             // 1マイクロ秒スリープ
